3abb275e1bfe440c6bdd2f1e28fe9b9dbf06d5d3
krshmt <tourekris16@gmail.com>
2024-04-23 16:42:28 +0200

    fin developpement portfolio. animatin a chaque changement de page

--- src/composants/Contact.tsx
+++ src/composants/Contact.tsx
@@ -1,14 +1,22 @@
 import Header from "./Header";
 import "./CSS/Header/HeaderNoir.css";
-function Contact (){
-    return (
-        <>
+import { motion } from "framer-motion";
+
+function Contact() {
+  return (
+    <>
+      <motion.div
+        transition={{ duration: 0.7 }}
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
         <div>
-            <h2>Contact</h2>
-            <p>Vous pouvez me contacter via mon adresse mail :</p>
+          <h2>Contact</h2>
+          <p>Vous pouvez me contacter via mon adresse mail :</p>
         </div>
-        </>
-    );
+      </motion.div>
+    </>
+  );
 }
 
 export default Contact;
--- src/composants/Moi.tsx
+++ src/composants/Moi.tsx
@@ -1,16 +1,33 @@
 import Header from "./Header";
 import "./CSS/Header/HeaderNoir.css";
+import { motion } from "framer-motion";
 
-function Moi () {
-    return (
-        <>
+function Moi() {
+  return (
+    <>
+      <motion.div
+        transition={{ duration: 0.7 }}
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
         <div>
-            <h1>À propos de moi</h1>
-            <p>Je suis un développeur web basé à Montréal, au Canada. J'ai une passion pour la création de sites Web et d'applications Web qui sont à la fois beaux et fonctionnels. J'ai une expérience dans une variété de technologies Web, y compris HTML, CSS, JavaScript, React et Node.js.</p>
-            <p>Je suis actuellement à la recherche de nouvelles opportunités de développement Web. Si vous avez un projet ou une idée que vous aimeriez discuter, n'hésitez pas à me contacter!</p>
+          <h1>À propos de moi</h1>
+          <p>
+            Je suis un développeur web basé à Montréal, au Canada. J'ai une
+            passion pour la création de sites Web et d'applications Web qui sont
+            à la fois beaux et fonctionnels. J'ai une expérience dans une
+            variété de technologies Web, y compris HTML, CSS, JavaScript, React
+            et Node.js.
+          </p>
+          <p>
+            Je suis actuellement à la recherche de nouvelles opportunités de
+            développement Web. Si vous avez un projet ou une idée que vous
+            aimeriez discuter, n'hésitez pas à me contacter!
+          </p>
         </div>
-        </>
-    );
+      </motion.div>
+    </>
+  );
 }
 
 export default Moi;
