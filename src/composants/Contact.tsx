e96a3fa01907951f2b60cbe0f33b4938ff3a5c98
krshmt <122864621+krshmt@users.noreply.github.com>
2024-04-27 17:28:02 +0200

    data-netlify="true"

--- src/composants/Contact.tsx
+++ src/composants/Contact.tsx
@@ -73,7 +73,7 @@ function Contact() {
         animate={{ y: 0, opacity: 1 }}
       >
         <main className="main__contact">
-          <form onSubmit={handleSubmit}>
+          <form data-netlify="true" onSubmit={handleSubmit}>
             <input
               type="email"
               value={email}
