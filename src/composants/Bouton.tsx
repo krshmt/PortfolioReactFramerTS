989e4619bec26118ae71936ed827a41f09e0d3c6
krshmt <tourekris16@gmail.com>
2024-06-27 15:55:20 +0200

    test

--- src/composants/Bouton.tsx
+++ src/composants/Bouton.tsx
@@ -1,15 +1,26 @@
 // @ts-nocheck
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./CSS/Bouton/Bouton.css";
 
-
 function Bouton() {
-  const [clickCount, setClickCount] = useState(0);
+  const [clickCount, setClickCount] = useState(() => {
+    // Récupérer le nombre de clics depuis localStorage, ou utiliser 0 s'il n'existe pas
+    const savedCount = localStorage.getItem('clickCount');
+    return savedCount !== null ? parseInt(savedCount, 10) : 0;
+  });
 
   const handleClick = () => {
-    setClickCount(clickCount + 1);
+    const newCount = clickCount + 1;
+    setClickCount(newCount);
+    // Enregistrer le nouveau nombre de clics dans localStorage
+    localStorage.setItem('clickCount', newCount);
   };
+
+  // Utilisation d'un effet pour enregistrer le nombre de clics lorsque le composant se démonte
+  useEffect(() => {
+    localStorage.setItem('clickCount', clickCount);
+  }, [clickCount]);
 
   return (
     <div className="bouton">
